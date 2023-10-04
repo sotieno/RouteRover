33b5d5eca97534833961a5b65e76d948f0738795
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-10-04 14:22:26 +0300

    Update get_route function

--- core/optimization/get_routes.py
+++ core/optimization/get_routes.py
@@ -8,7 +8,7 @@ import folium
 from flask import jsonify, request, render_template, redirect, url_for, flash
 from flask_login import login_required
 from core.optimization import get_route_bp
-from core.utils.functions import calculate_shortest_route, geocode_address
+from core.utils.functions import calculate_shortest_route, geocode_address, decode
 from shapely.geometry import Point
 
 
@@ -33,23 +33,25 @@ def get_route():
             flash('Start, end points and mode cannot be empty')
             return redirect(url_for('route.get_route_form'))
 
-        # Geocode the start and end points
-        start_location = tuple(reversed(geocode_address(start_point_coords)))
-        end_location = tuple(reversed(geocode_address(end_point_coords)))
+        # Geocode the start and end points in (longitude, latitude) format
+        start_location = geocode_address(start_point_coords)
+        end_location = geocode_address(end_point_coords)
 
-        # Ensure that the geocoded points are in SRID 4326
-        start_point_geom = Point(start_location[1], start_location[0])  # Assuming the tuple has (latitude, longitude) order
-        end_point_geom = Point(end_location[1], end_location[0])  # Assuming the tuple has (latitude, longitude) order
+        # Create a Point object for the starting location using longitude and latitude
+        start_point_geom = Point(start_location[0], start_location[1])
+
+        # Create a Point object for the ending location using longitude and latitude
+        end_point_geom = Point(end_location[0], end_location[1])
 
         if start_point_geom.is_valid and end_point_geom.is_valid:
-            # Execute Dijkstra's algorithm using pgrouting
-            map_file = calculate_shortest_route(
+            # Call function to calculate the route
+            display_route = calculate_shortest_route(
                 start_point_geom, end_point_geom, mode_of_travel=mode_of_travel)
 
-            if map_file != "":
+            if display_route != "":
                 # Render HTML template
                 # , folium_route=folium_route)
-                return render_template(map_file)
+                return render_template(display_route)
             else:
                 flash('No route found.')
                 return redirect(url_for('route.get_route_form'))
